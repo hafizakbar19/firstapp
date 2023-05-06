f88fcc0f84af3ec83f73b7484536e21adcf3772b
hafizakbar19 <hafizakbar19@gmail.com>
2023-05-07 02:03:12 +0500

    components added

---
+++ src/components/Promo.js
@@ -0,0 +1,14 @@
+function Promo(){
+    return (
+        <div className="promo-setion">
+            <div>
+                <h1>Don't miss the deal</h1>
+            </div>
+            <div>
+                <h2>Subscribe to my channel and get all of my items at 50% discount!</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Promo;
