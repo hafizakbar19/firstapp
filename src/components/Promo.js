229acee274abcb7befc34d2959735403af9bce21
hafizakbar19 <hafizakbar19@gmail.com>
2023-05-07 13:36:39 +0500

    costomized appjs

--- src/components/Promo.js
+++ src/components/Promo.js
@@ -1,11 +1,11 @@
-function Promo(){
+function Promo(props){
     return (
         <div className="promo-setion">
             <div>
                 <h1>Don't miss the deal</h1>
             </div>
             <div>
-                <h2>Subscribe to my channel and get all of my items at 50% discount!</h2>
+                <h2>Subscribe to my channel and get all of my items at 50% discount!, {props.name}, {props.color} </h2>
             </div>
         </div>
     );
