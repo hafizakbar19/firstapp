143dbf160bad3f8023b307522dbb03ee7c06bfa4
hafizakbar19 <hafizakbar19@gmail.com>
2023-05-09 17:51:02 +0500

    .

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav";
 import Promo from "./components/Promo";
+import Intro1 from "./components/Intro1";
 
 function App() {
   return (
     <div>
       <Nav />
       <Promo name="Ahmed" color="orange" />
+      <Intro1 />
     </div>
   );
 };
